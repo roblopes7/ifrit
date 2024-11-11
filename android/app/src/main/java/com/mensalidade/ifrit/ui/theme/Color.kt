5d2084d3e81d247a32a016b6959d1e132708bb63
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2024-11-11 17:52:54 -0300

    feat: login client + finalizado page cidades

--- android/app/src/main/java/com/mensalidade/ifrit/ui/theme/Color.kt
+++ android/app/src/main/java/com/mensalidade/ifrit/ui/theme/Color.kt
@@ -2,10 +2,61 @@ package com.mensalidade.ifrit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Light
+val PrimaryLight = Color(0xFFB60B0B)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFFBA3A3)
+val OnPrimaryContainerLight = Color(0xFF9B0202)
+
+val SecondaryLight = Color(0xFF535f70)
+val OnSecondaryLight = Color(0xFFffffff)
+val SecondaryContainerLight = Color(0xFFd7e3f7)
+val OnSecondaryContainerLight = Color(0xFF101c2b)
+
+val TertiaryLight = Color(0xFF6b5778)
+val OnTertiaryLight = Color(0xFFffffff)
+val TertiaryContainerLight = Color(0xFFf2daff)
+val OnTertiaryContainerLight = Color(0xFF251431)
+
+val ErrorLight = Color(0xFFba1a1a)
+val OnErrorLight = Color(0xFFffffff)
+val ErrorContainerLight = Color(0xFFffdad6)
+val OnErrorContainerLight = Color(0xFF410002)
+
+val BackgroundLight = Color(0xFFfdfcff)
+val OnBackgroundLight = Color(0xFF1a1c1e)
+val SurfaceLight = Color(0xFFfdfcff)
+
+val OutlineLight = Color(0xFF73777f)
+val SurfaceVariantLight = Color(0xFFdfe2eb)
+val OnSurfaceVariantLight = Color(0xFF43474e)
+
+// Dark
+val PrimaryDark = Color(0xFFC21515)
+val OnPrimaryDark = Color(0xFF8C0101)
+val PrimaryContainerDark = Color(0xFFB80202)
+val OnPrimaryContainerDark = Color(0xFFF8ADAD)
+
+val SecondaryDark = Color(0xFF535f70)
+val OnSecondaryDark = Color(0xFFffffff)
+val SecondaryContainerDark = Color(0xFFd7e3f7)
+val OnSecondaryContainerDark = Color(0xFF101c2b)
+
+val TertiaryDark = Color(0xFFd7bee4)
+val OnTertiaryDark = Color(0xFF3b2948)
+val TertiaryContainerDark = Color(0xFF523f5f)
+val OnTertiaryContainerDark = Color(0xFFf2daff)
+
+val ErrorDark = Color(0xFFffb4ab)
+val OnErrorDark = Color(0xFF690005)
+val ErrorContainerDark = Color(0xFF93000a)
+val OnErrorContainerDark = Color(0xFFffdad6)
+
+val BackgroundDark = Color(0xFF1a1c1e)
+val OnBackgroundDark = Color(0xFFe2e2e6)
+val SurfaceDark = Color(0xFF1a1c1e)
+
+val OutlineDark = Color(0xFF8d9199)
+val SurfaceVariantDark = Color(0xFF43474e)
+val OnSurfaceVariantDark = Color(0xFFc3c7cf)
