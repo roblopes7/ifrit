ec399a6561a0a4a1ec5fe0839a4856cbf47fc2f5
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2024-12-19 09:01:50 -0300

    fix: ajuste layout de login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,35 +1,43 @@
-
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ActivatedRoute, Router, RouterModule, RouterOutlet } from '@angular/router';
-
-import { CidadeComponent } from './pages/cidade/cidade.component';
-import { AuthService } from './pages/login/services/auth.service';
 import { ToastrService } from 'ngx-toastr';
-import { MatButtonModule } from '@angular/material/button';
+
+import { AuthService } from './pages/login/services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, CidadeComponent, RouterModule,
-    MatButtonModule
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    RouterModule,
+    MatButtonModule,
+    CommonModule
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ifrit-web-app';
+  showToolbar = true;
 
   constructor(
     private authService: AuthService,
     private router: Router,
     private route: ActivatedRoute,
     private toast: ToastrService
-  ) {}
+  ) {
+    this.router.events.subscribe(() => {
+      this.showToolbar = this.router.url !== '/login';
+    });
+  }
 
   logout() {
     this.authService.logout();
-    this.toast.success('Logout com sucesso', 'Logout', {timeOut: 7000})
+    this.toast.success('Logout com sucesso', 'Logout', { timeOut: 7000 });
     this.router.navigate(['login']);
   }
 }
